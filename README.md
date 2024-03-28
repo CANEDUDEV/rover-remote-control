9e08ba9a3cef50c122c05b9db16f4fecd7d1f5fa
Hashem Hashem <hashem@canedudev.com>
2024-03-28 13:37:49 +0100

    feat: gather steering wheel commands from client

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 Requirements for this project:
  - Raspberry pi 4 4gb with docker installed
+ - Windows PC with steering wheel
  - Tailscale on PC and rpi
+ - Universal Control Remapper (UCR) and vJoy for Windows
+
+Currently only the SuperDrive SV450 steering wheel is supported. To add support
+for other wheels, you need to create UCR profiles for them. The UCR profile
+configuration is the file `ucr/context.xml` and needs to be copied to the same
+folder as the UCR executable.
 
 1. Run `tailscale ip` on rpi, copy IPv4
 2. Run `docker compose up -d` on the rpi
--- static/client.js
+++ static/client.js
@@ -17,7 +17,7 @@ function createPeerConnection() {
   };
 
   //if (document.getElementById('use-stun').checked) {
-      config.iceServers = [{ urls: ['stun:rover:3478'] }];
+  config.iceServers = [{ urls: ['stun:rover:3478'] }];
   //}
 
   pc = new RTCPeerConnection(config);
@@ -229,4 +229,60 @@ function stop() {
   setTimeout(() => {
     pc.close();
   }, 500);
+}
+
+let gamepadInfo = document.getElementById("gamepad-info");
+
+// Check if Gamepad API is supported
+if ("getGamepads" in navigator) {
+  // Initialize gamepad state
+  let gamepad;
+
+  // Function to update gamepad state
+  function updateGamepadState() {
+    // Get gamepads
+    let detectedGamepads = navigator.getGamepads();
+    if (detectedGamepads.length < 1) {
+      gamepadInfo.innerHTML = "No gamepad detected.";
+      gamepad = null;
+      return;
+    }
+
+    // Assume only one gamepad
+    gamepad = detectedGamepads[0];
+  }
+
+  // Function to handle gamepad input
+  function handleGamepadInput() {
+    // Update gamepad state
+    updateGamepadState();
+
+    if (gamepad) {
+      // Clear previous gamepad info
+      gamepadInfo.innerHTML = "";
+
+      // Need to map 3 axes, 1 for the wheel and 1 for each pedal
+      if (gamepad.axes.length < 3) {
+        gamepadInfo.innerHTML = "Gamepad " + gamepad.id + " is not supported.";
+      }
+
+      // Display gamepad info
+      gamepadInfo.innerHTML += "<div>Gamepad " + gamepad.index + ": " + gamepad.id + "</div>";
+
+      let wheel = gamepad.axes[0]; // From -1 to 1, 0 is neutral
+      let right_pedal = gamepad.axes[1]; // From -1 to 1, -1 is neutral
+      let left_pedal = gamepad.axes[2]; // From -1 to 1, -1 is neutral
+
+      gamepadInfo.innerHTML += "<div>Wheel: " + wheel.toFixed(2) + "</div>";
+      gamepadInfo.innerHTML += "<div>Right pedal: " + right_pedal.toFixed(2) + "</div>";
+      gamepadInfo.innerHTML += "<div>Left pedal: " + left_pedal.toFixed(2) + "</div>";
+    }
+  }
+
+  // Set up gamepad input polling
+  setInterval(handleGamepadInput, 20); // Poll every 20ms
+
+} else {
+  // Gamepad API not supported
+  gamepadInfo.innerHTML = "Gamepad API not supported in this browser";
 }
