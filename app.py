482dbdaeb502d92a1148566e11c450f77471d231
Hashem Hashem <hashem@canedudev.com>
2024-04-05 12:02:09 +0200

    feat: send can messages

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import logging
+import subprocess
+import struct
 import ssl
 import uuid
 import json
@@ -10,6 +12,8 @@ from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaPlayer, MediaRelay
 from aiortc.rtcrtpsender import RTCRtpSender
 from aiohttp import web
+
+import can
 
 logger = logging.getLogger("pc")
 logging.basicConfig(level=logging.INFO)
@@ -64,19 +68,45 @@ async def offer(request):
 
     log_info("Created for %s", request.remote)
 
+    subprocess.run(["ip", "link", "set", "can0", "up", "type", "can", "bitrate", "125000"])
+
+    ch = can.Bus(interface="socketcan", channel="can0", bitrate=125000)
+
+    # Set SBUS receiver to silent mode
+    ch.send(can.Message(
+        arbitration_id=0,
+        dlc=8,
+        data=[4, 0, 0, 0x1, 0, 0, 0, 0],
+        is_extended_id=False,
+    ))
 
     @pc.on("datachannel")
     def on_datachannel(channel):
         @channel.on("message")
         def on_message(message):
-            if isinstance(message, str):
-                #log_info("datachannel: received %s", message)
-                pass
+            if message and isinstance(message, str):
+                frames = can_frames_from_message(message)
+                for frame in frames:
+                    try:
+                        ch.send(frame)
+                    except can.CanError:
+                        logger.error("Couldn't send CAN message %s", frame)
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
         log_info("Connection state is %s", pc.connectionState)
-        if pc.connectionState == "failed":
+        if pc.connectionState == "failed" or pc.connectionState == "closed":
+            # Set SBUS receiver to comm mode
+            ch.send(can.Message(
+                arbitration_id=0,
+                dlc=8,
+                data=[4, 0, 0, 0x3, 0, 0, 0, 0],
+                is_extended_id=False,
+            ))
+            ch.shutdown()
+
+            subprocess.run(["ip", "link", "set", "can0", "down"])
+
             await pc.close()
             pcs.discard(pc)
 
@@ -100,6 +130,53 @@ async def offer(request):
         ),
     )
 
+def can_frames_from_message(message):
+    cmd = json.loads(message)
+    if "wheel" not in cmd or "right_pedal" not in cmd or "left_pedal" not in cmd:
+        return []
+
+    wheel = cmd['wheel']
+    right_pedal = cmd['right_pedal']
+    left_pedal = cmd['left_pedal']
+
+    return [
+        create_steering_frame(wheel),
+        create_throttle_frame(right_pedal, left_pedal),
+    ]
+
+def create_steering_frame(wheel):
+    id = 0x100
+    dlc = 5
+    angle = float(wheel*45)
+    data = [1] + list(struct.pack("f", angle))
+    return can.Message(
+        arbitration_id=id,
+        data=data,
+        dlc=dlc,
+        is_extended_id=False,
+    )
+
+def create_throttle_frame(right_pedal, left_pedal):
+    # Pedals come in at values between -1 and 1,
+    # where -1 is fully depressed and 1 is fully pressed.
+    # We need to convert the pedal state to PWM pulses.
+    # Right pedal accelerates while left pedal brakes and reverses.
+    if left_pedal > -1:
+        # Brake pressed
+        pulse = 1500 + 500 * (left_pedal + 1) / 2
+    else:
+        pulse = 1500 - 500 * (right_pedal + 1) / 2
+
+    pulse = int(round(pulse))
+    id = 0x101
+    dlc = 5
+    data = [0] + list(pulse.to_bytes(2, "little", signed=True)) + [0, 0]
+    return can.Message(
+        arbitration_id=id,
+        data=data,
+        dlc=dlc,
+        is_extended_id=False,
+    )
 
 async def on_shutdown(_):
     # close peer connections
